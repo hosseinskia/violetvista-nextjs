c14c9480ce34b3021094657e262ffbd92927903e
Amir Hossein Kia <amirskia09@gmail.com>
2025-07-18 17:33:47 +0330

    append a static build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/violetvista-nextjs",
-  assetPrefix: "/violetvista-nextjs/",
-  trailingSlash: true,
 };
 
 export default nextConfig;
